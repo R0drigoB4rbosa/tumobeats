ecac0dffebf4afc56e8ef83ca8289dc672acfe92
R0drigoB4rbosa <rodrigo.barbosa@tumo.world>
2025-07-15 13:23:09 +0100

    aula 2

---
+++ script.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+
+    //Variaveis de elementos
+    let title = document.querySelector("#title");
+    let previousButton = document.querySelector("#previous-button");
+    let playButton = document.querySelector("#play-button");
+    let nextButton = document.querySelector("#next-button");
+
+
+
+
+    //funçoes
+    function changeTitle(value) {
+        title.innerText = value;
+    }
+
+    previousButton.onclick=function() {
+        console.log("previous button clicked");
+    }
+
+    playButton.onclick=function() {
+            console.log("play button clicked");
+    }
+
+    nextButton.onclick=function() {
+        console.log("next button clicked");
+    }
+
+
+
+    console.log(title);
+}
